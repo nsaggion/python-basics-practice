d4b5038315fe2ae9161590346e7444ffdceb5396
«Noah <saggionn@gmail.com>
2026-01-24 03:07:52 +0100

    Ejercicio en proceso

---
+++ bucles-loop/control_de_bucles/pass.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio de práctica en Python
+Tema: control_de_bucles
+Nivel: Básico
+"""
+"""
+Enunciado:
+Pide al usuario una cadena de texto (por ejemplo una palabra).
+Recorre cada carácter y:
+- Si el carácter es una vocal (a, e, i, o, u), no hagas nada (usa pass)
+- Si el carácter es una consonante, muestra la consonante por pantalla
+Al final, el programa habrá impreso solo las consonantes.
+"""
