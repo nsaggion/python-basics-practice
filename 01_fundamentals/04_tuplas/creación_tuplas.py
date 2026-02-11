274a9ab924afad49758ac798ecea1d580942bbf1
Noah <saggionn@gmail.com>
2026-02-11 01:31:32 +0100

    feat: ejercicio creación_tuplas completado

---
+++ 01_fundamentals/04_tuplas/creación_tuplas.py
@@ -0,0 +1,20 @@
+"""
+Ejercicio de práctica en Python
+Tema: Tupla
+Nivel: Básico
+"""
+""""
+Enunciado:
+- Crea una tupla con 5 ciudades.
+Muestra:
+- El primer elemento.
+- El último elemento.
+- El elemento en la posición 2.
+Muestra la cantidad de elementos que tiene la tupla.
+"""""
+
+ciudades = ("Barcelona","Milano","Buenos Aires","Hong Kong","Sídney")
+print("La primera ciudad és",ciudades[0],"y la ultima és",ciudades[-1])
+print("La cantidad de elementos és:",len(ciudades))
+
+
