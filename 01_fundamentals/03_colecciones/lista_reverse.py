c2d0018eead82c4bf93ea138e41833cb1e74d7b5
Noah <saggionn@gmail.com>
2026-02-11 01:13:25 +0100

    feat: ejercicio lista_reverse completado

---
+++ 01_fundamentals/03_colecciones/lista_reverse.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio de práctica en Python
+Tema: Listas
+Nivel: Básico
+"""
+""""
+Enunciado:
+- Crea una lista con 5 palabras (las que tú quieras).
+- Muestra la lista original.
+- Invierte el orden de los elementos usando el método reverse().
+- Muestra la lista invertida.
+"""""
+palabras = ["Nebulosa","Asteroide","Satélite","Galaxia","Órbita"]
+print("Lista de palabras original: ",palabras)
+palabras.reverse()
+print("Lista de palabras invertida:",palabras)
