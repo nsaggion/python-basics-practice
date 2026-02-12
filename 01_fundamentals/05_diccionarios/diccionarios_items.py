d0ba22fb36714b68ff249f9eaf630ff92057a9b8
Noah <saggionn@gmail.com>
2026-02-12 18:22:01 +0100

    feat: ejercicio diccionarios_items iniciado

---
+++ 01_fundamentals/05_diccionarios/diccionarios_items.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio de práctica en Python
+Tema: Diccionarios
+Nivel: Básico
+"""
+"""
+Crea un diccionario llamado auto con las siguientes claves:
+"marca"
+"modelo"
+"año"
+Asigna valores inventados.
+Recorre el diccionario usando items().
+Muestra cada elemento con el siguiente formato:
+La clave es ___ y su valor es ___
+"""
