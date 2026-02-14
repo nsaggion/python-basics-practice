6e998d3e8123e5f363845e432e5f63a003da5c3a
Noah <saggionn@gmail.com>
2026-02-14 02:21:01 +0100

    feat: ejercicio diccionarios_pop iniciado

---
+++ 01_fundamentals/05_diccionarios/diccionarios_pop.py
@@ -0,0 +1,20 @@
+"""
+Ejercicio de práctica en Python
+Tema: Diccionarios
+Nivel: Básico
+"""
+"""
+Crea un diccionario llamado usuario con las claves:
+"nombre"
+"email"
+"edad"
+"pais"
+Muestra el diccionario.
+Pide al usuario que ingrese una clave que quiera eliminar.
+Si la clave existe:
+- Elimínala usando pop()
+- Muestra el valor eliminado
+- Muestra el diccionario actualizado
+Si la clave no existe:
+- Muestra un mensaje de error.
+"""
