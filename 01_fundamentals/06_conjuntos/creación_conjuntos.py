f11e087b2e56ec2a04748b3b092946bd79592eac
Noah <saggionn@gmail.com>
2026-02-14 13:46:51 +0100

    feat: ejercicio creación_conjuntos resuelto

---
+++ 01_fundamentals/06_conjuntos/creación_conjuntos.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio de práctica en Python
+Tema: Conjuntos
+Nivel: Básico
+"""
+"""
+Crea un conjunto llamado numeros con los siguientes valores:
+- 1, 2, 3, 4, 4, 5, 2
+Muestra el conjunto por pantalla.
+Observa qué ocurre con los números repetidos.
+Muestra la cantidad de elementos que contiene el conjunto.
+"""
+numeros = {1,2,3,4,4,5,2}
+# Solo salen aquellos nuemros no repetidos.
+print(numeros)
+print(len(numeros))
