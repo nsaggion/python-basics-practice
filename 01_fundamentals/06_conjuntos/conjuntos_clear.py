d3054cba519ae0fde192cb7f6c0504da78676f17
Noah <saggionn@gmail.com>
2026-02-14 19:01:06 +0100

    feat: ejercicio conjuntos_pop & conjunto_clear iniciado

---
+++ 01_fundamentals/06_conjuntos/conjuntos_clear.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio de práctica en Python
+Tema: Conjuntos
+Nivel: Básico
+"""
+"""
+Enunciado:
+Crea un conjunto llamado paises con los siguientes valores:
+"España"
+"Italia"
+"Francia"
+"Alemania"
+Muestra el conjunto.
+Usa clear() para vaciar completamente el conjunto.
+Muestra el conjunto después de usar clear().
+"""
---
+++ 01_fundamentals/06_conjuntos/conjuntos_pop.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio de práctica en Python
+Tema: Conjuntos
+Nivel: Básico
+"""
+"""
+Enunciado:
+Crea un conjunto llamado letras con los siguientes valores:
+"a", "b", "c", "d"
+Muestra el conjunto.
+Usa pop() para eliminar un elemento del conjunto.
+Guarda el elemento eliminado en una variable.
+Muestra:
+- El elemento eliminado.
+- El conjunto actualizado.
+"""
