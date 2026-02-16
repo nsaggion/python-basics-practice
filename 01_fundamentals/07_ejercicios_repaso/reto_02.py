57f57565944ab1d9464c619ad9299287f2bd0504
Noah <saggionn@gmail.com>
2026-02-16 11:23:49 +0100

    feat: ejercicio repaso_02 resuelto

--- 01_fundamentals/07_ejercicios_repaso/reto_02.py
+++ 01_fundamentals/07_ejercicios_repaso/reto_02.py
@@ -14,3 +14,13 @@ Agrégalo al set.
 Muestra "Usuario registrado correctamente".
 El programa debe terminar cuando el usuario escriba "salir".
 """
+usuarios = set()
+texto = input("Porfavor ingrese un nombre de usuario: ")
+while texto != "salir":
+    if texto in usuarios:
+        print("Usuario ya registrado")
+    else:
+        usuarios.add(texto)
+        print("Usuario registrado correctamente")
+    texto = input("Porfavor ingrese un nombre de usuario: ")
+print("Usuarios registrados:", usuarios)
