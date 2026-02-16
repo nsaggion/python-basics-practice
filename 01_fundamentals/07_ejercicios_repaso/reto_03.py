a858910e821c95737e6e619e12524e6000c5fa40
Noah <saggionn@gmail.com>
2026-02-16 12:19:50 +0100

    feat: ejercicio repaso_03 resuelto

--- 01_fundamentals/07_ejercicios_repaso/reto_03.py
+++ 01_fundamentals/07_ejercicios_repaso/reto_03.py
@@ -15,3 +15,17 @@ Si no existe:
 - Muestra un mensaje de error.
 Al final, muestra el contenido del carrito.
 """
+productos = {"chicle":1,"manzanas":3,"pilas":2}
+carrito =[]
+print("Los productos són lo siguientres:")
+for producto in productos:
+    print(producto)
+producto = input("Por favor ingrese el nombre de un producto o tecle salir para finalizar: ")
+while producto != "salir" :
+    if producto in productos:
+        carrito.append(producto)
+        print("Producto agregado")
+    else:
+        print("Producto no existe")
+    producto = input("Por favor ingrese el nombre de un producto o tecle salir para finalizar: ")
+print("Los productos agrados al carrito són:",carrito)
