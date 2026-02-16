41623161bdb0d5f59b0087202ac1b4aa1c951848
Noah <saggionn@gmail.com>
2026-02-16 11:09:06 +0100

    feat: ejercicios repaso_01 resuelto

--- 01_fundamentals/07_ejercicios_repaso/reto_01.py
+++ 01_fundamentals/07_ejercicios_repaso/reto_01.py
@@ -8,8 +8,13 @@ Enunciado:
 Crea una lista con 5 nombres.
 Crea un diccionario vacío llamado notas.
 Recorre la lista con un for.
-
 Para cada nombre, pide al usuario que ingrese una nota.
 Guarda cada nombre como clave y la nota como valor en el diccionario.
 Al final, muestra el diccionario completo.
 """
+nombres = ["Andrea","Jorge","Sara","Miguel","Diana"]
+notas = {}
+for i in nombres:
+    nota = int(input("Ingrese una nota: "))
+    notas.update({i:nota})
+print("Diccionario con el nombre más las notas",notas)
