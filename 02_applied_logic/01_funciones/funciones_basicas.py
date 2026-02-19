eea946257cdf93a21b85ff8f31cd2d0168b06fb8
Noah <saggionn@gmail.com>
2026-02-19 17:01:53 +0100

    feat: comienzo applied_logic ejercicio funciones_basicas resuelto

---
+++ 02_applied_logic/01_funciones/funciones_basicas.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio de práctica en Python.
+Tema: Funciones.
+Nivel: Básico.
+"""
+"""
+Enunciado:
+Crea una función llamada saludar.
+Dentro de la función, imprime:
+- Hola, bienvenido al programa
+Llama a la función para que se ejecute.
+"""
+def saludar ():
+    print("Hola, bienvenido al programa.")
+saludar()
