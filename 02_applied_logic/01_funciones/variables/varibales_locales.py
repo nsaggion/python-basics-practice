21e13dee270c4f34a517042fcec175ef87810af3
Noah <saggionn@gmail.com>
2026-02-21 21:18:35 +0100

    feat: ejercicio variables_locales resuelto

---
+++ 02_applied_logic/01_funciones/variables/varibales_locales.py
@@ -0,0 +1,19 @@
+"""
+Ejercicio de práctica en Python.
+Tema: Varibales.
+Nivel: Medio.
+"""
+"""
+Crea una función llamada mostrar_mensaje.
+Dentro de la función:
+- Crea una variable llamada mensaje.
+- Asigna un texto cualquiera.
+Imprime la variable.
+Llama a la función.
+Luego, intenta imprimir mensaje fuera de la función.
+"""
+
+def mostrar_mensaje ():
+    mensaje = "Hola, como estas?"
+    print(mensaje)
+mostrar_mensaje()
