1cd423d2a0836cdf71a2115ca30b590f497d9f83
Noah <saggionn@gmail.com>
2026-02-21 21:37:19 +0100

    feat: ejercicio variables_globales&locales iniciado

---
+++ 02_applied_logic/01_funciones/variables/varibales_globales&locales.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio de práctica en Python.
+Tema: Varibales.
+Nivel: Medio.
+"""
+"""
+Crea una variable global llamada contador con valor 0.
+Crea una función llamada incrementar.
+Dentro de la función:
+- Incrementa el valor de contador en 1.
+- Llama a la función varias veces.
+Muestra el valor final de contador.
+"""
