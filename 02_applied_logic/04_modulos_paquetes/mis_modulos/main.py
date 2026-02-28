452f4f86523e2b3bed392f944fc9e18db7f341a6
Noah <saggionn@gmail.com>
2026-02-28 16:42:20 +0100

    feat: ejercicio main y operaciones resuelto

---
+++ 02_applied_logic/04_modulos_paquetes/mis_modulos/main.py
@@ -0,0 +1,19 @@
+"""
+Ejercicio de práctica en Python.
+Tema: Importacion.
+Nivel: Medio.
+"""
+"""
+Enunciado:
+Crea un programa que:
+Ejecute los modulos operaciones.
+Muestra por pantalla:
+- Suma.
+- Resta.
+- Mutiplicacion.
+"""
+import operaciones
+
+print(operaciones.sumar(5, 3))
+print(operaciones.restar(10, 4))
+print(operaciones.multiplicar(6, 7))
---
+++ 02_applied_logic/04_modulos_paquetes/mis_modulos/operaciones.py
@@ -0,0 +1,11 @@
+"""
+Arxhivo con funciones donde que resuelven operaciónes matematicas.
+"""
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
