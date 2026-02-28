f2b61958c4d10e560120ad8764146ec35254f528
Noah <saggionn@gmail.com>
2026-02-28 16:19:24 +0100

    fix: mejora logica ejercicio importacion_random

--- 02_applied_logic/04_modulos_paquetes/importacion_random.py
+++ 02_applied_logic/04_modulos_paquetes/importacion_random.py
@@ -15,21 +15,14 @@ Si no pide hasta que advine o se acaben los intentos"
 """
 import random
 
-def numUser():
-    num = int(input("Por favor ingrese un numero del 1 al 10, para adivinar el numero: "))
-    return num
-def numIncorrecto():
-    num = int(input("Por favor ingrese un numero del 1 al 10: "))
-    return num
-def numOtravez ():
-    num = int(input("Pruebe otravez: "))
-    return num
+def pedir_numero(mensaje):
+    return int(input(mensaje))
 intentos = 3
 numeroAleatorio = random.randint(1, 10)
-numUserio = numUser()
+numUserio = pedir_numero("Por favor ingrese un numero del 1 al 10: ")
 while True:
     if numUserio > 10 or numUserio < 1:
-        numUserio = numIncorrecto()
+        numUserio = pedir_numero("Número fuera de rango. Intente nuevamente: ")
         continue
     if numUserio == numeroAleatorio:
         print("¡Correcto! Adivinaste el número.")
@@ -40,4 +33,4 @@ while True:
             break
         print(f"Incorrecto. Le quedan un total de {intentos} intentos")
         intentos -=1
-    numUserio = numOtravez()
+    numUserio = pedir_numero("Pruebe otravez: ")
