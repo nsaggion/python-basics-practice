8cf8a3f6a6edc26f59f7377d122bf46675efd11d
Noah <saggionn@gmail.com>
2026-03-08 12:36:49 +0100

    feat: ejercicio main.py/calculadora.py parte 2 resuelto

---
+++ 02_applied_logic/04_modulos_paquetes/mis_modulos/ejercicio_02/main.py
@@ -0,0 +1,7 @@
+import calculadora
+
+resultado1 = calculadora.suma(10, 5)
+resultado2 = calculadora.multiplicar(4, 3)
+
+print("Resultado de la suma:", resultado1)
+print("Resultado de la multiplicación:", resultado2)
